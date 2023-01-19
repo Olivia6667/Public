bee4150110d14b42103c88d74e62e9e20c873974
Ekaterina Sosedina <122111814+Olivia6667@users.noreply.github.com>
2023-01-19 18:02:58 +0500

    Update Tickets 13.8.19.py

--- Tickets 13.8.19.py
+++ Tickets 13.8.19.py
@@ -7,10 +7,10 @@ while person <= tickets:
         print('Билет бесплатный')
     elif 25 > age_for_ticket >= 18:
         cash += 990
-            print(f"Стоимость билета: {cash} рублей")
+            print(f'Стоимость билета: {cash} рублей')
     else:
         cash += 1390
-        print(f"Стоимость билета: {cash} рублей")
+        print(f'Стоимость билета: {cash} рублей')
     person += 1
     if tickets > 3:
         sale = cash - ((cash/100) * 10)
