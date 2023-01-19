3d6825b2c89eae095261a128c9d3e99a2e318941
Ekaterina <ekaterina666@yandex-team.ru>
2023-01-19 18:38:14 +0500

    Update Tickets 13.8.19.py

--- Tickets 13.8.19.py
+++ Tickets 13.8.19.py
@@ -9,6 +9,7 @@ while person <= tickets:
         cash += 990
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         print("Стоимость билета: 990 рублей")
     else:
         cash += 1390
@@ -25,6 +26,12 @@ while person <= tickets:
         cash += 1390
         print(f"Стоимость билета: {cash} рублей")
 >>>>>>> parent of bee4150 (Update Tickets 13.8.19.py)
+=======
+        print("Стоимость билета: 990 рублей")
+    else:
+        cash += 1390
+        print("Стоимость билета: 1390 рублей")
+>>>>>>> parent of f5b76fe (Update Tickets 13.8.19.py)
     person += 1
     if tickets > 3:
         sale = cash - ((cash/100) * 10)
