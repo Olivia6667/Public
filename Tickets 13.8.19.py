4ba7f441d0659b87cb88247a2e720f06feaf7266
Ekaterina <ekaterina666@yandex-team.ru>
2023-01-19 18:42:09 +0500

    Revert "Update Tickets 13.8.19.py"
    This reverts commit ccb0d1abd2d8a47f523c7a9c7fa20543a8e26455.

--- Tickets 13.8.19.py
+++ Tickets 13.8.19.py
@@ -10,7 +10,6 @@ while person <= tickets:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
         print("Стоимость билета: 990 рублей")
     else:
         cash += 1390
@@ -33,12 +32,6 @@ while person <= tickets:
         cash += 1390
         print("Стоимость билета: 1390 рублей")
 >>>>>>> parent of f5b76fe (Update Tickets 13.8.19.py)
-=======
-            print(f"Стоимость билета: {cash} рублей")
-    else:
-        cash += 1390
-        print(f"Стоимость билета: {cash} рублей")
->>>>>>> parent of bee4150 (Update Tickets 13.8.19.py)
     person += 1
     if tickets > 3:
         sale = cash - ((cash/100) * 10)
